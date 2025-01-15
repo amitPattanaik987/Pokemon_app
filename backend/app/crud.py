16e3bae2d5304002741bad8d0515a3776b8901e0
Amit Pattanaik <161040099+amitPattanaik987@users.noreply.github.com>
2025-01-15 23:24:12 +0530

    Update crud.py

--- backend/app/crud.py
+++ backend/app/crud.py
@@ -9,7 +9,7 @@ def get_pokemons(db: Session):
 
 
 def create_pokemon(db: Session, pokemon: schemas.PokemonCreate):
-    db_pokemon = models.Pokemon(**pokemon.dict())  # Automatically handles the 'image' field
+    db_pokemon = models.Pokemon(**pokemon.dict()) 
     db.add(db_pokemon)
     db.commit()
     db.refresh(db_pokemon)
@@ -20,7 +20,7 @@ def update_pokemon(db: Session, pokemon_id: int, pokemon: schemas.PokemonUpdate)
     db_pokemon = get_pokemon(db, pokemon_id)
     if not db_pokemon:
         return None
-    for key, value in pokemon.dict().items():  # Automatically includes 'image' field if provided
+    for key, value in pokemon.dict().items():
         setattr(db_pokemon, key, value)
     db.commit()
     db.refresh(db_pokemon)
